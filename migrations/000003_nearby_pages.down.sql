8e3ab6d6f21f64d22f06ea97219ef5d414afca11
Johan Schuijt <johan.schuijt@ftm.nl>
2024-08-29 17:14:19 +0200

    add create prompt function, add embedding to prompt

---
+++ migrations/000003_nearby_pages.down.sql
@@ -0,0 +1,29 @@
+
+DROP FUNCTION create_prompt(query text, similarity_top_k int, embedding vector(1536));
+
+DROP VIEW prompts;
+
+ALTER TABLE private.prompts DROP COLUMN embedding;
+
+CREATE VIEW prompts WITH (security_invoker=true) AS
+ SELECT * FROM private.prompts;
+GRANT SELECT,INSERT ON TABLE prompts TO external_user;
+GRANT ALL ON TABLE prompts TO insight_worker;
+
+-- reset create_file function to json parameters
+DROP FUNCTION create_file(name text, parent_id bigint);
+
+CREATE FUNCTION create_file(json) RETURNS SETOF inodes
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    inode_id bigint;
+BEGIN
+    INSERT INTO inodes (name, parent_id, type) 
+        VALUES (($1->>'name')::text, ($1->>'parent_id')::bigint, 'file') 
+        RETURNING id INTO inode_id;
+    INSERT INTO files (inode_id) VALUES (inode_id);
+    RETURN QUERY SELECT * FROM inodes WHERE id=inode_id;
+END
+$$;
+GRANT ALL ON FUNCTION create_file(json) TO external_user;
--- migrations/000003_nearby_pages.up.sql
+++ migrations/000003_nearby_pages.up.sql
@@ -1,23 +1,52 @@
-CREATE OR REPLACE FUNCTION nearby_pages(embedding vector, owner_id int, similarity_top_k int)
-    RETURNS TABLE(id bigint, distance float8, contents text) 
+
+ALTER TABLE private.prompts ADD COLUMN embedding vector(1536);
+
+-- Rebuild prompts view to include embedding column
+DROP VIEW prompts;
+CREATE VIEW prompts WITH (security_invoker=true) AS
+ SELECT * FROM private.prompts;
+GRANT SELECT,INSERT ON TABLE prompts TO external_user;
+GRANT ALL ON TABLE prompts TO insight_worker;
+
+CREATE OR REPLACE FUNCTION create_prompt(query text, similarity_top_k int, query_embedding vector(1536))
+    RETURNS SETOF prompts
     LANGUAGE plpgsql
     AS $$
+DECLARE
+    prompt_id bigint;
 BEGIN
-    RETURN QUERY
-    SELECT 
-        p.id,
-        p.embedding <=> embedding AS distance,
-        p.contents
-    FROM 
-        Pages p
-    JOIN 
-        Inodes i ON p.inode_id = i.id
-    WHERE 
-        i.owner_id = owner_id
-        AND p.embedding IS NOT NULL
-    ORDER BY 
-        distance ASC
-    LIMIT 
-        similarity_top_k;
+    INSERT INTO prompts (query, similarity_top_k, embedding) 
+        VALUES (query, similarity_top_k, query_embedding)
+        RETURNING id INTO prompt_id;
+
+    INSERT INTO sources (prompt_id, page_id, similarity)
+        SELECT prompt_id, pages.id as page_id, pages.embedding <=> query_embedding AS similarity
+        FROM pages
+        WHERE pages.embedding IS NOT NULL
+        ORDER BY similarity ASC
+        LIMIT similarity_top_k;
+
+    RETURN QUERY SELECT * FROM prompts WHERE id=prompt_id;
 END;
 $$;
+
+GRANT ALL ON FUNCTION create_prompt(query text, similarity_top_k int, embedding vector(1536)) TO external_user;
+
+-- Switch function to direct parameters instead of json
+DROP FUNCTION create_file(json);
+
+CREATE FUNCTION create_file(name text, parent_id bigint) RETURNS SETOF inodes
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    inode_id bigint;
+BEGIN
+    INSERT INTO inodes (name, parent_id, type) 
+        VALUES (name, parent_id, 'file') 
+        RETURNING id INTO inode_id;
+    INSERT INTO files (inode_id) VALUES (inode_id);
+    RETURN QUERY SELECT * FROM inodes WHERE id=inode_id;
+END
+$$;
+
+GRANT ALL ON FUNCTION create_file(name text, parent_id bigint) TO external_user;
